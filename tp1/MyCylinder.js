9e100998ef250e75043102c2832e98737a2139eb
Lucas Calvet Santos <lucascalvet2000@hotmail.com>
2022-10-20 19:34:29 +0100

    fix: cylinder normals

--- tp1/MyCylinder.js
+++ tp1/MyCylinder.js
@@ -1,4 +1,4 @@
-import {CGFobject} from '../lib/CGF.js';
+import { CGFobject } from '../lib/CGF.js';
 
 
 /**
@@ -20,27 +20,27 @@ export class MyCylinder extends CGFobject {
         this.vertices = [];
         this.indices = [];
         this.normals = [];
-        this.texCoords=[];
+        this.texCoords = [];
 
         var texmap = 0;
-        var texmapIncrement = 1/this.slices;
+        var texmapIncrement = 1 / this.slices;
 
         let sliceAng = 0;
         let sliceAngIncrement = 2 * Math.PI / this.slices;
         let stackIncrement = this.height / this.stacks;
         let radiusDecrement = (this.bRadius - this.tRadius) / this.stacks;
+        let normalZ = (this.bRadius-this.tRadius)/this.height
 
-
-        for(let i = 0; i <= this.stacks; i++){
-            let currentHeight = i*stackIncrement;
-            let currentRadius = this.bRadius - i*radiusDecrement;
-            for(let i = 0; i < this.slices; i++){
+        for (let i = 0; i <= this.stacks; i++) {
+            let currentHeight = i * stackIncrement;
+            let currentRadius = this.bRadius - i * radiusDecrement;
+            for (let i = 0; i < this.slices; i++) {
 
                 let x = currentRadius * Math.cos(sliceAng);
                 let y = currentRadius * Math.sin(sliceAng);
-    
+
                 this.vertices.push(x, y, currentHeight);
-                this.normals.push(x, y, currentHeight);
+                this.normals.push(Math.cos(sliceAng), Math.sin(sliceAng), normalZ);
                 this.texCoords.push(texmap, this.height);
                 this.texCoords.push(texmap, 0); //TODO: check this
 
@@ -50,14 +50,14 @@ export class MyCylinder extends CGFobject {
         }
 
         // setup indices
-        for(let stack = 0; stack < this.stacks; stack++){
-            for(let slice = 0; slice < this.slices; slice++){
-                let offset = stack*this.slices;
+        for (let stack = 0; stack < this.stacks; stack++) {
+            for (let slice = 0; slice < this.slices; slice++) {
+                let offset = stack * this.slices;
 
                 let x1 = offset + slice;
-                let y1 = offset + (slice+1)%this.slices;
-                let z1 = offset + slice+this.slices;
-                this.indices.push(x1,y1,z1);
+                let y1 = offset + (slice + 1) % this.slices;
+                let z1 = offset + slice + this.slices;
+                this.indices.push(x1, y1, z1);
 
                 let x2 = (offset + this.slices) + slice;
                 let y2 = offset + (slice + 1) % this.slices;
@@ -70,7 +70,7 @@ export class MyCylinder extends CGFobject {
         this.initGLBuffers();
     }
 
-    updateBuffers(complexity){
+    updateBuffers(complexity) {
         this.slices = 3 + Math.round(9 * complexity); //complexity varies 0-1, so slices varies 3-12
 
         // reinitialize buffers
@@ -79,22 +79,21 @@ export class MyCylinder extends CGFobject {
     }
 
     setFillMode() {
-        this.primitiveType=this.scene.gl.TRIsliceAngS;
+        this.primitiveType = this.scene.gl.TRIsliceAngS;
     }
 
-    setLineMode()
-    {
-        this.primitiveType=this.scene.gl.LINE_STRIP;
+    setLineMode() {
+        this.primitiveType = this.scene.gl.LINE_STRIP;
     }
 
     /**
-	 * @method updateTexCoords
-	 * Updates the texture coordinates of the component
-	 * @param length_s - Texture scale factor for the s axis
-	 * @param length_t - Texture scale factor for the t axis
-	 */
-	updateTexCoords(length_s, length_t) {
-		// this.texCoords = [];
-		this.updateTexCoordsGLBuffers();
-	}
+     * @method updateTexCoords
+     * Updates the texture coordinates of the component
+     * @param length_s - Texture scale factor for the s axis
+     * @param length_t - Texture scale factor for the t axis
+     */
+    updateTexCoords(length_s, length_t) {
+        // this.texCoords = [];
+        this.updateTexCoordsGLBuffers();
+    }
 }
